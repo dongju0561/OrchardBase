ef38a395a387bee3f8d5cc3fb65d90ae6f12f53c
Dongju Park <pdj95044@gmail.com>
2022-09-26 19:12:53 +0900

    6th commit Feat: 전등 상태 변화 체크 기능

--- Hub/raspBase.py
+++ Hub/raspBase.py
@@ -7,7 +7,28 @@ config = {
 	"storageBucket": "orchardbase.appspot.com"
 }
 
-firebase = pyrebase.initialize_app(config)
+firebase = pyrebase.initialize_app(config) #firebase와 라즈베리파이 연동을 위한 초기화
 db = firebase.database()
 
-db.child("light").child("light1").set("false")
+#데이터 베이스 최신화 
+oldData1 = db.child("light").child("light1").get().val() # get()메소드를 이용해서 조회
+oldData2 = db.child("light").child("light2").get().val()
+oldData3 = db.child("light").child("light3").get().val()
+
+def checkChangingLightState(light, oldDataP):
+	global oldData
+	newData = db.child("light").child(light).get()
+	if newData.val() != oldDataP:
+		print(light+" is changed!")
+		print(oldData)
+		oldData = newData.val()
+
+
+while True:
+	checkChangingLightState("light1", oldData1)
+	checkChangingLightState("light2", oldData2)
+	checkChangingLightState("light3", oldData3)
+
+
+# db.child("light").child("light1").set("False") / 키값  light/light1의 값 변경하는 방법
+
