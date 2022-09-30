381e2151f0bb77a2bf25fd6e065dd995aee34466
Dongju Park <pdj95044@gmail.com>
2022-09-30 19:09:57 +0900

    11th Feat: 온도 설정방식 변경에 따른 fetch방식 변경

--- Hub/raspBase.py
+++ Hub/raspBase.py
@@ -16,9 +16,8 @@ db = firebase.database()
 oldLight1 = ""
 oldLight2 = ""
 oldLight3 = ""
-oldAirDownTemp = "False"
-oldAirUpTemp = "False"
-oldAirPower = "False"
+oldAirPower = ""
+oldDTemp = "23" # desiredTemp
 
 print("Start program")
 
@@ -28,61 +27,69 @@ def initState(): #edit: initState
 	global oldLight1
 	global oldLight2
 	global oldLight3 
-
+	global oldAirPower
+	global oldDTemp 
 	oldLight1 = db.child("light").child("light1").get().val() # get()메소드를 이용해서 조회
 	oldLight2 = db.child("light").child("light2").get().val()
 	oldLight3 = db.child("light").child("light3").get().val()
-	db.child("Airconditioner").child("downTemp").set("False") 
-	db.child("Airconditioner").child("power").set("False") 
-	db.child("Airconditioner").child("upTemp").set("False") 
+	oldAirPower = db.child("Airconditioner").child("power").get().val()
+	oldDTemp = db.child("Airconditioner").child("dTemp").get().val()
 
 # 전등 상태 변화 확인 함수
 def changeState(control, component, oldData):
-	print("changeState")		
+	newData = db.child(control).child(component).get().val()
 	if control == "light":
-		newData = db.child("light").child(component).get().val()
-
 		if newData != oldData:
 			print(component+" is changed!")
+			operateBLU(newData)
 			return newData
 		else: 						# 위에 조건을 만족 못하고 따로 return을 안시켜주면 None값을 return함...
 			return oldData 				# 위의 조건을 만족 못할 시
+	elif control == "Airconditioner" and component == "dTemp":
+		if newData != oldData:
+			print(component+" is changed!")
+			operateBLU(newData)
+			return newData
+		else:
+			return oldData
 
-	elif control == "airconditioner":
-		newData = db.child("Airconditioner").child(component).get().val()
+	elif control == "Airconditioner" and component == "dTemp":
 
 		if newData != oldData:
 			print(component+" is changed!")
-			operateAir(component)
+			operateBLU(newData)
 			return oldData
 		else:
 			return oldData
 
-def operateAir(AirComponent):
-	operateBLU()
-	db.child("Airconditioner").child(AirComponent).set("False") 
+	elif control == "Airconditioner" and component == "power":
+		if newData != oldData:
+			print(component+" is changed!")
+			operateBLU(newData)
+			return newData
+		else: 
+			return oldData
 
-def operateBLU():
-	print("send data")
+def operateBLU(value):
+	print(value + "send data")
 
 def checkState():
-	print("checkState")
 	global oldLight1	
 	global oldLight2
 	global oldLight3
 	global oldAirDownTemp
 	global oldAirUpTemp
 	global oldAirPower
+	global oldDTemp
 
-	state = db.child("state").get().val()
-	print(state)
+	state = db.child("state").get().val() # state변수가 바뀌었을때 일괄적으로 component 전체 확인
+
 	if state == "true":
 		oldLight1 = changeState("light", "light1", oldLight1)
 		oldLight2 = changeState("light","light2", oldLight2)
 		oldLight3 = changeState("light","light3", oldLight3)
-		oldAirDownTemp = changeState("airconditioner", "downTemp", oldAirDownTemp)
-		oldAirUpTemp = changeState("airconditioner","upTemp", oldAirUpTemp)
-		oldAirPower = changeState("airconditioner","power", oldAirPower)
+		oldDTemp = changeState("Airconditioner","dTemp", oldDTemp)
+		oldAirPower = changeState("Airconditioner","power", oldAirPower)
 		db.child("state").set("False")
 # main
 initState()
