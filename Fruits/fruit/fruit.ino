0d608aca16bafe33634f160192e361310278a591
DongjuPark <pdj95044@gmail.com>
2022-11-06 01:17:02 +0900

    test gasDector and servo motor

--- Fruits/fruit/fruit.ino
+++ Fruits/fruit/fruit.ino
@@ -1,17 +1,39 @@
 #include <SoftwareSerial.h>
+#include <Servo.h>
+
+#define sensorPin A0
 
 SoftwareSerial bluetooth(2,3);
+
+int smokeLevel;
 
 void setup(){
   Serial.begin(9600);
   bluetooth.begin(9600);
+  pinMode(sensorPin,INPUT);
 }
 
 void loop(){
-  if(bluetooth.available()){
-    Serial.write(bluetooth.read());
+  smokeLevel = analogRead(sensorPin);
+  Serial.println(smokeLevel);
+
+  delay(100);
+  if (smokeLevel > 300) {
+    bluetooth.write(0x01);
+    delay(200);
   }
-  if(Serial.available()){
-    bluetooth.write(Serial.read());
+  else {
+    bluetooth.write("n");
+    delay(200);
   }
 }
+  // //bluetooth에서 어떤 신호가 왔을때 시리얼 모니터에 출력
+  // if(bluetooth.available()){
+  //   Serial.write(bluetooth.read());
+  // }
+  // //시리얼 모니터에서 어떤 입력이 들어왔을때 bluetooth에 입력
+  // if(Serial.available()){
+  //   bluetooth.write(Serial.read());
+  // }
+
+// 98:da:60:03:82:04
